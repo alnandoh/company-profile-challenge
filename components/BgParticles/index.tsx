134f1059aa605c5070c1fdb6f3558da1e074306e
alnandoh <alnandoharsono@gmail.com>
2024-05-21 00:40:56 +0700

    Turning on the particles on mobile test

--- components/BgParticles/index.tsx
+++ components/BgParticles/index.tsx
@@ -14,7 +14,7 @@ export default function BgParticles() {
 
   return (
     <Particles
-      className="w-full h-full fixed -z-[1] hidden md:block"
+      className="w-full h-full fixed -z-[1]"
       id="tsparticles"
       init={particlesInit}
       loaded={particlesLoaded}
